d1b0acb5b235d3777ed873021709c3696f478613
fulchr <cfulton@dunwoody.edu>
2019-02-21 10:45:20 -0600

    adding new example

---
+++ chapt5_ex/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace chapt5_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 1, b = 2, c = 5, d = 4, e = 6, f = 7;
+            int j = 2, k= 5, m = 6, n=9;
+        //Letter B
+        //    while(d<f){
+        //        d = d + 1;
+        //        e = e - 1;
+        //    }
+        //    WriteLine("The value for d is: " + d + " The value for e is : " +e + " The value for f is: " + f);
+
+        //Letter E
+        while(j < k){  //2 < 5
+            while(m < n){ //6 < 9
+                WriteLine("Hello");
+                m = m+1;
+            }
+            j = j+1;
+        }
+           
+        }//end of main
+    }
+}
---
+++ string_console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace string_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string marvelMovie = "The Avengers Infinity War";
+           string myFav = "I don't Know";
+
+
+        Console.WriteLine($"{marvelMovie} is the best Marvel Movie, but {myFav} why!");
+        }
+    }
+}
