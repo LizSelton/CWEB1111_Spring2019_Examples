52c85ea375cfe1103639ed97e73e393e5014e7fb
cifulton <cfulton@dunwoody.edu>
2019-04-08 14:29:39 -0500

    adding hr_payroll application

---
+++ chapt_eight_ec/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace extraCredit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //DECLATATIONS
+            double numberA;
+            double numberB;
+            double EXIT= 00.00;
+            double primer;
+
+        //INPUTS
+        System.Console.WriteLine("Thank you for entering the program");
+        System.Console.WriteLine("Please enter your first number");
+        numberA = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine("Please enter your second number");
+        numberB = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine("To add please type '1'. To subtract please type '2'.");
+        primer = Convert.ToDouble(Console.ReadLine());
+
+            //Housekeeping
+            while(numberA != EXIT){
+                getChoice(numberA, numberB, primer);
+            //    sum( numberA, numberB);
+            //    difference(numberA, numberB);
+                
+
+            System.Console.WriteLine("To exit, please type '00.00'. to continue please type your first number");
+            numberA = Convert.ToDouble(Console.ReadLine());
+                if(numberA != EXIT){
+                System.Console.WriteLine("Please enter your second number");
+                numberB = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+
+    
+        }
+        static double sum(double numA, double numB){
+            double sum = numA + numB;
+            System.Console.WriteLine($"{numA} + {numB} = {sum}");
+            return sum;
+           }
+        static double difference(double numC, double numD){
+            double subtraction = numC - numD;
+            System.Console.WriteLine($"{numC} - {numD} = {subtraction}");
+            return subtraction;
+
+        }
+        static void getChoice(double numE, double numF,  double primer){
+           
+            if (primer == 1){
+                sum(numE, numF);
+            }else{
+                difference(numE, numF);
+            }
+        }
+    }
+}
+
+
+
+
---
+++ hr_payroll/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace hr_payroll
+{
+    class hr_department
+    {
+        static void Main(string[] args)
+        {
+            //DECLARATIONS
+            string employeeName;
+            int deptNum, numbOfHoursWorked;
+            double hourlyRate;
+            string EXIT = "EXIT";
+
+            string[] DEPART_NAMES = new String[7];
+
+            DEPART_NAMES[0] = "Personnel";
+            DEPART_NAMES[1] = "Marketing";
+            DEPART_NAMES[2] = "Manufacturing";
+            DEPART_NAMES[3] = "Computer Science";
+            DEPART_NAMES[4] = "Sales";
+            DEPART_NAMES[5] = "Accounting";
+            DEPART_NAMES[6] = "Shipping";
+
+
+            double [] deptPayout = {0,0,0,0,0,0,0};
+
+            employeeName = welcome();
+
+            //Continous Iteration  to allow end-user to enter employees until Sentinel is entered
+            do{
+                //get department module
+                deptNum = getDepartment(employeeName, DEPART_NAMES);
+                System.Console.WriteLine(deptNum);
+
+
+
+
+                
+
+            }while(employeeName != EXIT);
+
+        }//end of main
+
+        static string welcome(){
+            string name;
+
+            //SEQUENCE - Intro. and getting initial priming value
+            System.Console.WriteLine("Welcome, this program allows you to input employee data and produce a report of how much each department will pay in salary");
+            System.Console.WriteLine("Please input your name");
+            name = Console.ReadLine();
+            return name;
+
+        }
+
+        static int getDepartment(string name, string[] DEPT){
+            int deptNum;
+            //Looping structure - iterate through DEPT names for end-user knows which department to select
+            for(var i = 0; i < DEPT.Length; i++){
+                System.Console.WriteLine($"Please enter {i} for {DEPT[i]}");
+            }
+            System.Console.WriteLine($"{name} please enter your deparment number from the list above");
+            deptNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            return deptNum;
+        }
+    }
+}
